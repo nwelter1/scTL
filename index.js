7983c9d25abd5bab68f5c1cd17d2d012e22e719b
nwelter1 <nwelter1@gmail.com>
2023-01-07 14:23:55 -0700

    reformat

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const puppeteer = require('puppeteer');
-function delay(time){
+
+function delay(time) {
     return new Promise(function(resolve) {
-        setTimeout(resolve,time)
+        setTimeout(resolve, time)
     });
 }
 const getTracks = async (title) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto("https://www.1001tracklists.com/")
-    await page.screenshot({path: 'test.png'})
-    await page.setViewport({width: 1920, height: 1001 })
+    await page.screenshot({
+        path: 'test.png'
+    })
+    await page.setViewport({
+        width: 1920,
+        height: 1001
+    })
     await page.waitForSelector('input#sBoxInput')
     await page.click("input#sBoxInput")
     console.log(`trying to type title: ${title}`)
@@ -17,9 +23,13 @@ const getTracks = async (title) => {
     await page.waitForSelector('.acObjLi')
     await page.click('.acObjLi')
     await delay(5000)
-    await page.screenshot({ path: '1001.png' })
-    const tracklistContent = await page.evaluate(() => 
-        Array.prototype.slice.call(document.getElementsByClassName('trackValue')).map(function(x){ return x.innerText })
+    await page.screenshot({
+        path: '1001.png'
+    })
+    const tracklistContent = await page.evaluate(() =>
+        Array.prototype.slice.call(document.getElementsByClassName('trackValue')).map(function(x) {
+            return x.innerText
+        })
     );
     console.log(tracklistContent)
     browser.close()
@@ -29,18 +39,14 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
-
-
-
 app.get('/', async (request, response) => {
     let title = request.body.title;
     console.log(`Title: ${title}`)
     let tracks = await getTracks(title);
     response.send(JSON.stringify(tracks))
 
-    
-})
 
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
